40b0d78fff0116a8db1170410d5be460a6ccf85c
nathan03VT <nathan03@vt.edu>
2024-09-14 14:31:42 -0400

    ONNX YAY

--- cvPlayground/model/handClassifier/keypoint_classifier.py
+++ cvPlayground/model/handClassifier/keypoint_classifier.py
@@ -1,33 +1,22 @@
 import numpy as np
-import tensorflow as tf
-
+import onnxruntime as ort
 
 class KeyPointClassifier(object):
     def __init__(
         self,
-        model_path='model/handClassifier/keypoint_classifier.tflite',
+        model_path='model/handClassifier/keypoint_classifier.onnx',
         num_threads=1,
     ):
-        self.interpreter = tf.lite.Interpreter(model_path=model_path,
-                                               num_threads=num_threads)
-
-        self.interpreter.allocate_tensors()
-        self.input_details = self.interpreter.get_input_details()
-        self.output_details = self.interpreter.get_output_details()
+        self.session = ort.InferenceSession(model_path, providers=['CPUExecutionProvider'])
+        self.input_name = self.session.get_inputs()[0].name
+        self.output_name = self.session.get_outputs()[0].name
 
     def __call__(
         self,
         landmark_list,
     ):
-        input_details_tensor_index = self.input_details[0]['index']
-        self.interpreter.set_tensor(
-            input_details_tensor_index,
-            np.array([landmark_list], dtype=np.float32))
-        self.interpreter.invoke()
-
-        output_details_tensor_index = self.output_details[0]['index']
-
-        result = self.interpreter.get_tensor(output_details_tensor_index)
+        input_data = np.array([landmark_list], dtype=np.float32)
+        result = self.session.run([self.output_name], {self.input_name: input_data})
 
         result_index = np.argmax(np.squeeze(result))
 
