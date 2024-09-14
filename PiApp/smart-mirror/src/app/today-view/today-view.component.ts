bef92c3dde512501c81d310c48fdb10039e446fe
Mason DiGiorgio <mason@digiorgiofamily.net>
2024-09-13 20:52:19 -0400

    Initial angular commit

---
+++ PiApp/smart-mirror/src/app/today-view/today-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-today-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './today-view.component.html',
+  styleUrl: './today-view.component.css'
+})
+export class TodayViewComponent {
+
+}
---
+++ PiApp/smart-mirror/src/app/weather-view/weather-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-view.component.html',
+  styleUrl: './weather-view.component.css'
+})
+export class WeatherViewComponent {
+
+}
