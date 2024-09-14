ff1823e0eff373847f7a4f4445a618c5eb4e0890
Mason DiGiorgio <mason@digiorgiofamily.net>
2024-09-14 01:39:31 -0400

    Add carousel and finger swiping

--- PiApp/smart-mirror/src/app/app.component.ts
+++ PiApp/smart-mirror/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WeatherViewComponent } from './weather-view/weather-view.component';
 import { provideHttpClient } from '@angular/common/http';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WeatherViewComponent],
+  imports: [RouterOutlet, CarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
